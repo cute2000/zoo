e455914f70adacfe1388e0cdf3a6eb4f7eecb651
vinson <763537842@qq.com>
2021-10-27 23:24:25 +0800

    Create lm_bd.js

---
+++ lm_bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e9bb1e3ac7a73d85fbf35a7041162389";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
