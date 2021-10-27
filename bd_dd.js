97eca14ab2f857368d78f05718afd8d24c2cf287
vinson <763537842@qq.com>
2021-10-27 23:52:10 +0800

    Create bd_dd.js

---
+++ bd_dd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?544fe06fe96b5005f5d951e4d1ea7e93";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
