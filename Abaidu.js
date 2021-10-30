d44acd53310ef3cf6a62e0b3bc78bcb57404be42
vinson <763537842@qq.com>
2021-10-30 15:46:01 +0800

    Create Abaidu.js

---
+++ Abaidu.js
@@ -0,0 +1,8 @@
+// all
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3fb716c68839bc6ae8ef5484bd86e14c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
